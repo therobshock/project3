aeee289a6d8a30708337e1dc4b09996c498cbf21
Zak Clark <zak@zakariahclark.com>
2019-06-22 11:17:50 -0700

    Working add item survey

--- get-your-shit-together/src/App.js
+++ get-your-shit-together/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
+// import SurveyCreator from "./SurveyCreator";
+import ItemQuestions from "./Components/ItemQuestions";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "jquery-ui/themes/base/all.css";
+import "nouislider/distribute/nouislider.css";
+import "select2/dist/css/select2.css";
+import "bootstrap-slider/dist/css/bootstrap-slider.css";
+
+import "jquery-bar-rating/dist/themes/css-stars.css";
+
+import $ from "jquery";
+import "jquery-ui/ui/widgets/datepicker.js";
+import "select2/dist/js/select2.js";
+import "jquery-bar-rating";
+
+import * as widgets from "surveyjs-widgets";
+
+import "icheck/skins/square/blue.css";
+window["$"] = window["jQuery"] = $;
+require("icheck");
+
+Survey.StylesManager.applyTheme("default");
+
+widgets.icheck(Survey, $);
+widgets.select2(Survey, $);
+widgets.inputmask(Survey);
+widgets.jquerybarrating(Survey, $);
+widgets.jqueryuidatepicker(Survey, $);
+widgets.nouislider(Survey);
+widgets.select2tagbox(Survey, $);
+widgets.signaturepad(Survey);
+widgets.sortablejs(Survey);
+widgets.ckeditor(Survey);
+widgets.autocomplete(Survey, $);
+widgets.bootstrapslider(Survey);
+
+class App extends Component {
+
+  render() {
+    Survey.Survey.cssType = "bootstrap";
+    var model = new Survey.Model(this.json);
+    return (
+      <div className="App">
+        <ItemQuestions />
+        <div id="yourScore"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ get-your-shit-together/src/Components/ItemQuestions.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+// import logo from "../logo.svg";
+import * as Survey from "survey-react";
+
+
+export class ItemQuestions extends Component  {
+ //Define Survey JSON
+ json = {
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+        {
+        "type": "text",
+        "name": "itemname",
+        "title": "What item would you like to get rid of? "
+        },
+        {
+        "type": "rating",
+        "name": "question1",
+        "title": "How important is this item to you"
+       },
+       {
+        "type": "rating",
+        "name": "question2",
+        "title": "How much would you cry if you lost it?"
+       },
+       {
+        "type": "rating",
+        "name": "question3",
+        "title": "How much would you cry if you lost it?"
+       }
+      ]
+     }
+    ],
+    "showCompletedPage": false
+   }
+   
+  
+   //Define a callback methods on survey complete
+   onComplete(survey) {
+    //Write survey results into database
+    console.log("Survey results: " + JSON.stringify(survey.data));
+    console.log((survey.data)); //lets play with this to see how we can choose just the integer itself
+    
+    //This counts the number of questions
+
+    var q1 = (survey.data.question1);
+    var q2 = (survey.data.question2);
+    var q3 = (survey.data.question3);
+    console.log("Answers to questions: " + q1, q2, q3);
+
+    var totalOfQuestionScores = q1 + q2 + q3;
+    var averageOfQuestions = totalOfQuestionScores / 3;
+    console.log("Average" + averageOfQuestions);
+
+    document.getElementById("yourScore").innerHTML = "<h3>Your score for " + "<strong>" + survey.data.itemname + "</strong> is " + averageOfQuestions +"</h3>";
+    
+
+
+   }
+   render() {
+    //Create the model and pass it into react Survey component
+    //You may create survey model outside the render function and use it in your App or component
+    //The most model properties are reactive, on their change the component will change UI when needed.
+    var model = new Survey.Model(this.json);
+    return (<Survey.Survey model={model} onComplete={this.onComplete}/>);
+    /*
+    //The alternative way. react Survey component will create survey model internally
+    return (<Survey.Survey json={this.json} onComplete={this.onComplete}/>);
+    */
+    //You may pass model properties directly into component or set it into model
+    // <Survey.Survey model={model} mode="display"/>
+    //or 
+    // model.mode="display"
+    // <Survey.Survey model={model}/>
+    // You may change model properties outside render function. 
+    //If needed react Survey Component will change its behavior and change UI.
+   }
+}
+
+export default ItemQuestions;
